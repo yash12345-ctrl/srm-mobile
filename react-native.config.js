d55bf1768ebcd9af16848d3fba2542ec360c5d24
yash12345-ctrl <yashtomar12345y@gmail.com>
2026-05-07 10:24:13 +0530

    Fix EAS build doctor checks

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    'react-native-worklets': {
+      platforms: {
+        android: null, // disables autolinking on Android
+        ios: null,     // disables autolinking on iOS
+      },
+    },
+  },
+};
